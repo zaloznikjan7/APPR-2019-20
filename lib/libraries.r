7f39ac6c0e8f35f578021010b1c9aaaac0888e05
zaloznikjan7 <zaloznikjan@yahoo.com>
2020-03-25 22:27:46 +0100

    Dodal par knjižnic

--- lib/libraries.r
+++ lib/libraries.r
@@ -3,6 +3,10 @@ library(rvest)
 library(gsubfn)
 library(tidyr)
 library(shiny)
+library(ggplot2)
+library(dplyr)
+library(plotrix)
+library(plotly)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
